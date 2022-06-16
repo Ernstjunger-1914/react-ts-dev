3618a0b94edeb54e0667d12707d70bd83c582298
asdf <vot768@naver.com>
2022-06-16 11:04:19 +0900

    1. Loading 컴포넌트 생성

---
+++ src/components/atoms/Loading.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const LoadingSpin = keyframes`
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+`;
+
+const LoadingBackground = styled.body`
+    background-color: #f9f9fa;
+`;
+
+const LoadingDiv = styled.div`
+    border: 5px solid rgba(18, 65, 145, 255);
+    border-radius: 50%;
+    border-top: 5px solid #fff;
+    width: 40px;
+    height: 40px;
+    animation: ${LoadingSpin} 1s linear infinite;
+    margin: auto;
+    left: 0;
+    right: 0;
+    top: 0px;
+    bottom: 0;
+    position: fixed;
+`;
+
+function Loading() {
+    return (
+        <LoadingBackground>
+            <LoadingDiv />
+        </LoadingBackground>
+    );
+}
+
+export default Loading;
