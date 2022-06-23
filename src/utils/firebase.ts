f300db6e87bb56fa1730d8201dbe04f04af5bbf9
asdf <vot768@naver.com>
2022-06-23 11:20:41 +0900

    [edit] 1. firebase 관련 설정 파일 변경

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,26 +1,28 @@
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
 
 const {
-    REACT_APP_APIKEY,
-    REACT_APP_AUTHDOMAIN,
-    REACT_APP_PROJECTID,
-    REACT_APP_STORAGEBUCKET,
-    REACT_APP_MESSAGINGSENDERID,
-    REACT_APP_APPID,
-    REACT_APP_MEASUREMENTID
+    REACT_APP_FIREBASE_APIKEY,
+    REACT_APP_FIREBASE_AUTHDOMAIN,
+    REACT_APP_FIREBASE_PROJECTID,
+    REACT_APP_FIREBASE_STORAGEBUCKET,
+    REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    REACT_APP_FIREBASE_APPID,
+    REACT_APP_FIREBASE_MEASUREMENTID
 } = process.env;
 
 const firebaseConfig = {
-  apiKey: REACT_APP_APIKEY,
-  authDomain: REACT_APP_AUTHDOMAIN,
-  projectId: REACT_APP_PROJECTID,
-  storageBucket: REACT_APP_STORAGEBUCKET,
-  messagingSenderId: REACT_APP_MESSAGINGSENDERID,
-  appId: REACT_APP_APPID,
-  measurementId: REACT_APP_MEASUREMENTID
+  apiKey: REACT_APP_FIREBASE_APIKEY,
+  authDomain: REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: REACT_APP_FIREBASE_APPID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENTID
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
+export const FirebaseAuth = getAuth(app);
