e935510d328c1e7a7175123b16b98d5cdfffef3e
손소독 <vot768@naver.com>
2022-06-24 23:12:46 +0900

    [update] 1. log 클래스 내용 추가(build시 발생하는 에러 수정 필요)

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -11,7 +11,7 @@ enum Log_Level_E {
     Verbose = 'verbose'
 };
 
-class Log {
+class Logger {
 
     public static readonly LogLevel: string = Log_Level_E.Debug;
     public static readonly MaxLogFileSize: number = 1024 * 1024 * 10;
@@ -30,7 +30,7 @@ class Log {
 
     private createLogDir() {
         try {
-            mkdirp.sync(Log.LogPath);
+            mkdirp.sync(Logger.LogPath);
         } catch(e) {
             console.log(`Create log path FAILED; ${e}`);
             return;
@@ -46,6 +46,79 @@ class Log {
         if(this.writer !== undefined) {
             return this.writer;
         }
+
+        return winston.createLogger({
+            transports: [
+                new winston.transports.File({
+                    filename: path.join(Logger.LogPath, "./server.log"),
+                    level: Logger.LogLevel,
+                    maxFiles: Logger.MaxLogFiles,
+                    maxsize: Logger.MaxLogFileSize,
+                    format: winston.format.printf(info => `${this.getTimeStampFormat()} [${info.level.toUpperCase()}] ${info.message}`),
+                    tailable: true
+                }),
+                new winston.transports.Console({
+                    level: Logger.LogLevel,
+                    format: winston.format.printf(info => `${this.getTimeStampFormat()} [${info.level.toUpperCase()}] ${info.message}`)
+                }),
+            ]
+        });
+    }
+
+    private createFinalMessage(message: string) {
+        let stackInfo = this.getStackInfo(1);
+        let fileNameInfo: string = (Logger.UseRelativePath ? stackInfo?.relativePath : stackInfo?.file) as string;
+        let finalMessage: string = `[${fileNameInfo}:${stackInfo?.line}] ${message}`;
+
+        return finalMessage;
+    }
+
+    /**
+     * @description Error
+     */
+     public e(...args: any[]) {
+        this.writer.error(this.createFinalMessage(this.getLogString(args)));
+    }
+
+    /**
+     * @description Warn
+     */
+    public w(...args: any[]) {
+        this.writer.warn(this.createFinalMessage(this.getLogString(args)));
+    }
+
+    /**
+     * @description Info
+     */
+     public i(...args: any[]) {
+        this.writer.info(this.createFinalMessage(this.getLogString(args)));
+    }
+
+    /**
+     * @description Debug
+     */
+    public d(...args: any[]) {
+        this.writer.debug(this.createFinalMessage(this.getLogString(args)));
+    }
+
+    /**
+     * @description Verbose
+     */
+    public v(...args: any[]) {
+        this.writer.verbose(this.createFinalMessage(this.getLogString(args)));
+    }
+
+    private getLogString(args: any[]) {
+        let resultString: string = "";
+
+        for(let i=0; i<args.length; i++) {
+            if(typeof(args[i]) === 'object') {
+                resultString += JSON.stringify(args[i]) + "\t";
+            } else {
+                resultString += args[i] + "\t";
+            }
+        }
+        return args[0] + "\t" +resultString;
     }
     
     private getStackInfo(stackIndex: number) {
@@ -64,7 +137,7 @@ class Log {
         if(sp && sp.length === 5) {
             return {
                 method: sp[1],
-                relativePath: path.relative(Log.ProjectRootPath, sp[2]),
+                relativePath: path.relative(Logger.ProjectRootPath, sp[2]),
                 line: sp[3],
                 pos: sp[4],
                 file: path.basename(sp[2]),
@@ -75,4 +148,5 @@ class Log {
 
 }
 
+const Log: Logger = new Logger();
 export default Log;
