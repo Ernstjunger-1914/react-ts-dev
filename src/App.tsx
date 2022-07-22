79bbbc059746fbf93b891771dee897b2c2d9bb83
asdf <vot768@naver.com>
2022-07-22 16:21:39 +0900

    1. Header 컴포넌트 구성 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React, { Suspense } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import 'styles/scss/App.scss';
 import { Loading } from 'components/molecules';
-import { Route, Switch } from 'react-router-dom';
 import { LoginPage, Mainpage, NotFoundPage } from 'components/pages';
+import { Header } from 'components/organisms';
 
 function App() {
   return (
     <>
       <Suspense fallback={<Loading />}>
+        <Header />
         <Switch>
           <Route exact path='/' component={Mainpage} />
           <Route path='/login' component={LoginPage} />
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,13 +1,22 @@
+import { Link } from 'components/atoms';
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
 
 /**
  * @description Header 컴포넌트
  */
 function Header() {
     return (
-        <div>
-            
-        </div>
+        <StyledHeader>
+            <p>SSD</p>
+            <Link path='/'>Home</Link>
+        </StyledHeader>
     );
 }
 
