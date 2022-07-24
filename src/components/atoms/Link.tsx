906cac674fc28b2fb843812f9d35cc7d1b36c83f
손소독 <vot768@naver.com>
2022-07-24 19:13:38 +0900

    1. Link 컴포넌트 css 수정

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Link as ReactRouterLink } from 'react-router-dom';
 import styled from 'styled-components';
+import { Colors } from 'utils/colors';
 
-const StyledLink = styled(ReactRouterLink)({
-    textDecoration: 'none'
-}) as ReactRouterLink;
+const StyledLink = styled(ReactRouterLink)`
+    align-items: center;
+    color: ${Colors.black};
+    display: flex;
+    text-decoration: none;
+    height: 100%;
+    cursor: pointer;
+
+    &:active {
+        color: ${Colors.lightgray};
+    }
+`;
 
 /**
  * @description Link 컴포넌트
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,11 +1,26 @@
 import { Link } from 'components/atoms';
 import React from 'react';
 import styled from 'styled-components';
+import { Colors } from 'utils/colors';
 
 const StyledHeader = styled.header`
+    background: ${Colors.white};
+    height: 80px;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    padding: 0.5rem calc((100vw - 1000px) / 2);
+    z-index: 10;
+`;
+
+const MenuDiv = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: 24px;
+    
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
 `;
 
 /**
@@ -14,8 +29,10 @@ const StyledHeader = styled.header`
 function Header() {
     return (
         <StyledHeader>
-            <p>SSD</p>
-            <Link path='/'>Home</Link>
+            <Link path='/'>SSD</Link>
+            <MenuDiv>
+                <Link path='/'>Home</Link>
+            </MenuDiv>
         </StyledHeader>
     );
 }
