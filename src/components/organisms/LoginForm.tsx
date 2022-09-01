00334fce3711e8dfec9ba9d90b11c13127c195d0
asdf <vot768@naver.com>
2022-09-01 17:48:45 +0900

    1. LoginForm 컴포넌트 구성 중(입력창과 버튼이 가로로 나열되는 문제 수정 필요)

--- src/components/organisms/LoginForm.tsx
+++ src/components/organisms/LoginForm.tsx
@@ -1,13 +1,35 @@
+import { Button, Input, Span } from 'components/atoms';
 import React from 'react';
 import styled from 'styled-components';
+import { Colors } from 'utils/colors';
 
-const Wrapper = styled.div`
-    margin: 0 auto;
+const StyledWrapper = styled.div`
     align-items: center;
     display: flex;
+    flex-grow: 1;
     flex-direction: column;
     justify-content: center;
     width: 25rem;
+    margin: 0 auto;
+`;
+
+const StyledTitle = styled.div`
+    margin-bottom: 30px;
+`;
+
+const StyledLoginForm = styled.div`
+    width: 400px;
+    display: flex;
+`;
+
+const StyledInput = styled.div`
+    margin-bottom: 15px;
+`;
+
+const StyledButton = styled.div`
+    margin-right: 50px;
+    margin-left: 50px;
+    width: calc(100% - 100px);
 `;
 
 /**
@@ -15,9 +37,39 @@ const Wrapper = styled.div`
  */
 function LoginForm() {
     return (
-        <Wrapper>
-            
-        </Wrapper>
+        <StyledWrapper>
+            <StyledTitle>
+                <Span
+                    fontSize={50}
+                    fontWeight='bolder'
+                >
+                    SSD
+                </Span>
+            </StyledTitle>
+            <StyledLoginForm>
+                <StyledInput>
+                    <Input
+                        height={2}
+                        type='email'
+                        placeholder='Email'
+                    />
+                </StyledInput>
+                <StyledInput>
+                    <Input
+                        height={2}
+                        type='password'
+                        placeholder='Password'
+                    />
+                </StyledInput>
+                <StyledButton>
+                    <Button>
+                        <Span>
+                            로그인
+                        </Span>
+                    </Button>
+                </StyledButton>
+            </StyledLoginForm>
+        </StyledWrapper>
     );
 }
 
